bb9c3ac02dc0d458a1a0f455c2fbf017b24c2f93
Sammy <valdivinosammyvp@gmail.com>
2026-09-18 18:37:28 -0300

    take n°2...

--- index.js
+++ index.js
@@ -145,8 +145,8 @@ async function deletar(esse, card) {
 
 btnDeslogarIndex.addEventListener("click", () => {
     signOut(auth).then(() => {
-        formDiv.style.display = "none";
-        location.reload();
+        // formDiv.style.display = "none";
+        window.location.reload();
     }).catch(error => {
         console.log("houve algum erro:", error)
     });
